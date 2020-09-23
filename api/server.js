75f82b926d7d2c47efe1bc5fc92f456744bce68f
he zhang <a73841959@gmail.com>
2020-09-23 21:18:36 +0800

    ink

---
+++ api/server.js
@@ -0,0 +1,22 @@
+import Cors from 'cors'
+import initMiddleware from './init-middleware'
+const SourceQuery = require('sourcequery');
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+module.exports = async (req, res) => {
+  await cors(req, res)
+  const {
+    query: { ip },
+  } = req
+		const query=new SourceQuery(ip,27015,1000,true)
+		var queryinfo = await query.getInfo();
+		response.push(JSON.parse(JSON.stringify(queryinfo)))
+  res.status(200).json({ response })
+}
