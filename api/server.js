eed22cd93e1781c948991c6b42b4cc1258d87d50
he zhang <a73841959@gmail.com>
2020-09-26 23:59:42 +0800

    fix

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 import Cors from 'cors'
 import initMiddleware from './init-middleware'
-const SourceQuery = require('sourcequery');
+const Server = require('@fabricio-191/valve-server-query');
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -16,9 +16,33 @@ module.exports = async (req, res) => {
     query: { ip },
   } = req
   let data=[]
-		const query=new SourceQuery(ip,27015,1000,true)
-		query.getInfo().then(info =>data.push(info))
-    query.getPlayers().then(players => data.push(players))
-    query.close()
+  const server = new Server({
+    ip: ip,
+    port: 27015,
+    timeout: 2000
+});
+ 
+server.on('ready', () => {
+    //emitted when the server is ready, and you can start doing queries 
+    console.log('I am ready!')
+})
+ 
+//if you do a query before the server is ready, it will be delayed until it is ready
+//the maximum time for the server to be ready is the "timeout" you set, multiplied by 1.333 (a 33% more)
+ 
+server.getInfo()
+.then(info => {
+    data.push(info)
+
+})
+.catch(console.error)
+ 
+server.getPlayers()
+.then(players => {
+    data.push(players)
+})
+.catch(console.error)
+ 
+
   res.status(200).json(data)
 }
