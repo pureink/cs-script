00b1c6653a65318cc1b6b49272b2c4ddf8c08a96
pureink <a73841959@gmail.com>
2020-12-28 12:21:40 +0800

    fix

--- api/server.js
+++ api/server.js
@@ -1,29 +1,23 @@
-import Cors from 'cors'
-import initMiddleware from './init-middleware'
-const SourceQuery = require('sourcequery');
-function utfconvert(string){
-  var buf = Buffer.from(string,'latin1');
-  return buf.toString('utf8')
-}
-// Initialize the cors middleware
+import Cors from "cors";
+import initMiddleware from "./init-middleware";
+const SourceQuery = require("sourcequery");
 const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ["GET", "POST", "OPTIONS"],
   })
-)
-
+);
 module.exports = async (req, res) => {
-  await cors(req, res)
+  await cors(req, res);
   const {
-    query: { ip },
-  } = req
-    const query=new SourceQuery(ip,27015,1000,true)
-    var queryinfo = await query.getInfo();
-    var player =await query.getPlayers()
-    for(let i=0;i<player.length;i++){
-    	player[i].name=utfconvert(player[i].name)
-    }
-  res.status(200).json({queryinfo,player})
-}
+    query: { ip, port },
+  } = req;
+  const query = new SourceQuery(ip, port || 27015, 1000, true);
+  var origininfo = await query.getInfo();
+  var player = await query.getPlayers();
+  var server = {};
+  server.name = origininfo.name;
+  server.map = origininfo.map;
+  server.players = origininfo.players;
+  server.maxplayers = origininfo.maxplayers;
+  res.status(200).json({ server, player });
+};
